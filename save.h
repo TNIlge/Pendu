8dcd98a6ad5f493b7b7a5960b68284a75fa9a68b
Mathieu TENE <tenemathieu1@gmail.com>
2025-12-15 13:00:37 +0100

    implémentation des modules user, stats, leaderboard, et save. Orchestration dans le main

--- save.h
+++ save.h
@@ -5,13 +5,8 @@
 
 // Prototypes des fonctions de save.c
 int sauvegarder_partie(Partie *partie);
-Partie* charger_partie(void);
-int partie_sauvegardee_existe(void);
-void supprimer_sauvegarde(void);
-Statistiques* charger_statistiques(void);
-int sauvegarder_statistiques(Statistiques *stats);
-void ajouter_partie_aux_stats(Statistiques *stats, Partie *partie);
-void calculer_moyennes(Statistiques *stats);
-void initialiser_statistiques(Statistiques *stats);
+Partie* charger_partie(int id_utilisateur);
+int partie_sauvegardee_existe(int id_utilisateur);
+void supprimer_sauvegarde(int id_utilisateur);
 
 #endif
